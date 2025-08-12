36283e3fad986eed3c3cf142e91728aec12c0ffc
Vital Fadeev <vital.fadeev@gmail.com>
2025-08-12 09:32:45 +0700

    upd

--- source/app.d
+++ source/app.d
@@ -1,9 +1,7 @@
 import std.stdio;
-import core.stdc.stdlib        : EXIT_SUCCESS,EXIT_FAILURE;
-import std.format              : format;
+import core.stdc.stdlib : EXIT_SUCCESS,EXIT_FAILURE;
 import wayland_struct;
 import impl;
-import util;
 
 
 //extern (C)
@@ -19,6 +17,7 @@ main () {
 
     // EVENT LOOP
     foreach (Event* event; wayland.events) {
+        writeln (*event);
         switch (event.type) {
             case Event.Type.POINTER_BUTTON: 
                 if (event.pointer.button == BTN_LEFT)
@@ -30,46 +29,9 @@ main () {
                 break;
             default:
         }
-        writeln (*event);
     }
 
     wayland.cleanup ();
 
     return EXIT_SUCCESS;
 }
-
-//void 
-//main () {
-//	// INIT
-//    auto wayland   = Wayland ();
-
-//    auto c         = wayland.connect (false,null);
-//    auto screen    = c.screen;
-//    auto window    = c.window (screen);
-
-//    writeln ("width x height (in pixels): ", screen.width_in_pixels, "x", screen.height_in_pixels);
-//    writeln ("hwnd: ",window);
-
-//    // EVENT LOOP
-//    foreach (event; c.events) {
-//        switch (event.type) {
-//            case Event.Type.KEY_PRESS   : 
-//            case Event.Type.KEY_RELEASE : 
-//                writeln (event.keyboard);
-//                break;
-//            case Event.Type.BUTTON_PRESS   : 
-//            case Event.Type.BUTTON_RELEASE : 
-//                writeln (event.button);
-//                break;
-//            case Event.Type.MOTION_NOTIFY : 
-//                writeln (event.motion);
-//                break;
-//            case Event.Type.EXPOSE : 
-//                writeln (event.expose);
-//                break;
-//            default : writeln (event);
-//        }        
-//    }
-
-//    c.disconnect ();
-//}
